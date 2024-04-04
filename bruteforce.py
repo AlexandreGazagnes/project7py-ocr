d794d876be758091c08c329ee8afbf06dfb5033a
AlexandreGazagnes <a_syoez@yahoo.fr>
2024-04-04 12:37:07 +0200

    udpate

--- bruteforce.py
+++ bruteforce.py
@@ -12,25 +12,55 @@ import pandas as pd
 
 
 def find_best_solution(df, maximum=500.00):
-    
 
-    #Action A
-    print("\n Action A \n")
-    resultat = 0
-    i=0
-    while i != range(len(df)) and resultat < maximum and df["value"][i] < 110:
-        print(f"Investir {df["value"][i]} dans {df["action_name"][i]}")
-        resultat += df["value"][i]
-        print(resultat)
-        i += 1
-    print("\nLa solution n°1 est la suivante : Le résultat doit être inférieure à 500 euros et la valeur doit être inférieure à 110 euros.\n")
-    print(f"Total de l'investissement: {resultat}")
-    
-    solution = ["action_A", "action_B", "action_C"]
+    # oN VA TESTER DE FAIRE DES PORTEFEUILLES QUE DE 1 ACTION
 
-    logging.critical("solution " + str(solution))
+    all_candidats = list(df.action_name.values)
 
-    return solution
+    # filtrer par 500
+
+    candidats_inf_500 = list()
+    for name in all_candidats:
+        val = df.loc[df.action_name == name, "value"].iloc[0]
+        # logging.warning(val)
+        if val <= maximum:
+            candidats_inf_500.append(name)
+
+    logging.warning(f"candidats_inf_500 is {candidats_inf_500}")
+
+    # Tri par gain
+    _df = df.sort_values("new_value", ascending=False)
+
+    # filtre les 500 Max par gain
+    _df = _df.loc[df.action_name.isin(candidats_inf_500)]
+
+    return _df.iloc[0].action_name, _df.iloc[0].new_value
+
+    # ON VA TESTER DE FAIRE TOUS LES PORTEFEUILLES DE 2 ACTIONS
+
+    # oN VA TESETER DE FAIRE TOUS LES PORTEUEILLES DE 3 ACTIONS ...
+
+    # ETC ETC
+
+    # ON VA TESTER DE PRENDRE TOUTES LES ACTIONS
+
+    # #Action A
+    # print("\n Action A \n")
+    # resultat = 0
+    # i=0
+    # while i != range(len(df)) and resultat < maximum and df["value"][i] < 110:
+    #     print(f"Investir {df["value"][i]} dans {df["action_name"][i]}")
+    #     resultat += df["value"][i]
+    #     print(resultat)
+    #     i += 1
+    # print("\nLa solution n°1 est la suivante : Le résultat doit être inférieure à 500 euros et la valeur doit être inférieure à 110 euros.\n")
+    # print(f"Total de l'investissement: {resultat}")
+
+    # solution = ["action_A", "action_B", "action_C"]
+
+    # logging.critical("solution " + str(solution))
+
+    # return solution
 
 
 def return_csv_file():
@@ -42,19 +72,19 @@ def return_csv_file():
         # read file
         file_ = csv.reader(csvf, delimiter=",")
 
-        # parse the file
-        li = []
-        for i, data in enumerate(file_):
+    # parse the file
+    li = []
+    for i, data in enumerate(file_):
 
-            if not i:
-                continue
+        if not i:
+            continue
 
-            # logging
-            logging.warning(data)
-            logging.warning(type(data))
+        # logging
+        # logging.warning(data)
+        # logging.warning(type(data))
 
-            # add to li
-            li.append((data[0], float(data[1]), float(data[2])))
+        # add to li
+        li.append((data[0], float(data[1]), float(data[2])))
 
     return li
 
@@ -91,6 +121,8 @@ def main():
     df = use_pandas()
     df = compute_values(df)
     solution = find_best_solution(df)
+
+    print(f"$$$$$$$$$$$$$$$$$$$$ {solution} $$$$$$$$$$$$$$$$$$f")
 
 
 if __name__ == "__main__":
